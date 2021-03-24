6064cd8c593c9d311c4bddb590788b6d64307098
Jane <janewu0828@gmail.com>
2021-03-24 12:40:43 +0800

    enhance code

--- interview_questions/leetcode/python/src/leetcoding_challenge/week-1_august_1st_august_7th/3409/detect_capital.py
+++ interview_questions/leetcode/python/src/leetcoding_challenge/week-1_august_1st_august_7th/3409/detect_capital.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+import re
+
 class Solution(object):
     def detectCapitalUse(self, word):
         """
         :type word: str
         :rtype: bool
         """
+        # if not word:
+        #     return False
+
+        # return word.isupper() | word.islower() | (word == word.capitalize())
+
+        # leetcode
+        # Ref: https://leetcode.com/problems/detect-capital/solution
         if not word:
             return False
 
-        return word.isupper() | word.islower() | (word == word.capitalize())
+        return re.fullmatch("[A-Z]+|[A-Z]?[a-z]+", word)
